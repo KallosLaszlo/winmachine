1a1336b7069d16323470766bb41c95daf9e28dad
Kallós László <kallos.laszlo@bardiauto.hu>
2026-04-07 15:06:45 +0200

    v0.1.5: Bump version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,7 +40,7 @@ function App() {
           </button>
         </div>
         <div className="sidebar-footer">
-          <span className="version">v0.1.4</span>
+          <span className="version">v0.1.5</span>
         </div>
       </nav>
       <main className="content">
