815c330dbcc3970b9d209fccd1665fa78b886099
Kallós László <kallos.laszlo@bardiauto.hu>
2026-04-07 16:07:20 +0200

    chore: Bump version to v0.1.8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,7 +40,7 @@ function App() {
           </button>
         </div>
         <div className="sidebar-footer">
-          <span className="version">v0.1.5</span>
+          <span className="version">v0.1.8</span>
         </div>
       </nav>
       <main className="content">
