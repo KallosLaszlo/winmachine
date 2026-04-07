e5b32786944cbd2ff0358e9928c63c1b75e0d0e6
Kallós László <kallos.laszlo@bardiauto.hu>
2026-04-07 16:06:32 +0200

    feat: Add single instance mutex to prevent multiple app instances

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"os"
+	"syscall"
+	"unsafe"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -15,7 +18,36 @@ var assets embed.FS
 //go:embed build/windows/icon.ico
 var trayIcon []byte
 
+var (
+	kernel32     = syscall.NewLazyDLL("kernel32.dll")
+	createMutexW = kernel32.NewProc("CreateMutexW")
+)
+
+const ERROR_ALREADY_EXISTS = 183
+
+// ensureSingleInstance creates a named mutex to prevent multiple instances
+func ensureSingleInstance() (syscall.Handle, bool) {
+	name, _ := syscall.UTF16PtrFromString("Global\\WinMachineBackupApp")
+	handle, _, err := createMutexW.Call(0, 1, uintptr(unsafe.Pointer(name)))
+	if handle == 0 {
+		return 0, false
+	}
+	// err contains GetLastError() result from the syscall
+	if err.(syscall.Errno) == ERROR_ALREADY_EXISTS {
+		syscall.CloseHandle(syscall.Handle(handle))
+		return 0, false
+	}
+	return syscall.Handle(handle), true
+}
+
 func main() {
+	mutexHandle, ok := ensureSingleInstance()
+	if !ok {
+		// Another instance is already running
+		os.Exit(0)
+	}
+	defer syscall.CloseHandle(mutexHandle)
+
 	app := NewApp()
 	app.trayIcon = trayIcon
 
